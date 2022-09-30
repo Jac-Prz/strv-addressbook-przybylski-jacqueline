c02be79769beee25b1ce50fd41cb7868e042eac5
unknown <jac.prz@live.com>
2022-09-30 09:07:17 +0200

    refresh

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -8,24 +8,27 @@ const handleRefreshToken = async (req, res) => {
     const refreshToken = cookies.jwt;
 
     //find user with same refresh token
-    const currentUser = await User.findOne({ refreshToken });
-    if (!currentUser) return res.sendStatus(403);
+    const currentUser = await User.findOne({ refreshToken })
+    if (currentUser) {
+        jwt.verify(
+            refreshToken,
+            process.env.JWT_REFRESH_TOKEN_SECRET,
+            (err, decodedInfo) => {
+                if (err || currentUser.username) return res.sendStatus(403);
+                const accessToken = jwt.sign({
+                    'email': decodedInfo.email
+                },
+                    process.env.JWT_ACCESS_TOKEN_SECRET,
+                    { expiresIn: '60s' }
+                );
+                res.json({ accessToken })
+            }
+        )
+    }
+    else return res.sendStatus(403);
 
     //check that refresh token jwts match
-    jwt.verify(
-        refreshToken,
-        process.env.JWT_REFRESH_TOKEN_SECRET,
-        (err, decodedInfo) => {
-            if (err || currentUser.username) return res.sendStatus(403);
-            const accessToken = jwt.sign({
-                'email': decodedInfo.email
-            },
-                process.env.JWT_ACCESS_TOKEN_SECRET,
-                { expiresIn: '60s' }
-            );
-            res.json({ accessToken })
-        }
-    )
+    
 }
 
 module.exports = { handleRefreshToken };
